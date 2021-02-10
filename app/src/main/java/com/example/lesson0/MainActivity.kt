2c751247529cd8bf9cd8902be5b20def4a81e09e
Slava <kergakovslava@gmail.com>
2021-02-10 14:32:13 +0200

    first commit

---
+++ app/src/main/java/com/example/lesson0/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.lesson0
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.text.set
+import com.example.lesson0.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun showMess(view: View) {
+        textViewOutput.text=editTextTextPersonName.text
+    }
+
+
+}
